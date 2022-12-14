249a065fc5ba62d941455af23e4632b72e7d2964
Miguel Zavala <miguelzavalac@gmail.com>
2022-12-13 19:26:41 -0600

    feat(busan-tree): add icons and some render conditionals

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { Select, Tooltip } from "sancho";
 import { css } from '@emotion/css'
 import { styles } from './styles';
+import { Data } from '../../mocks/data';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faUser,
@@ -10,51 +11,75 @@ import {
   faCaretDown,
   faInfo,
   faPlus,
-  faCaretUp
+  faRemove,
+  faCaretUp,
+  faEdit
 } from '@fortawesome/free-solid-svg-icons'
 
 const options = [
   {
     type: 'Developer',
+    isAdmin:false,
     allocation : 1000,
     id:1,
     icon: faCode,
   },
   {
     type: 'QA-Tester',
+    isAdmin:false,
     allocation : 500,
     id:2,
     icon: faGear
   },
   {
     type: 'Manager',
+    isAdmin:true,
+    items: [],
     allocation : 300,
     id:3,
     icon: faUser
   },
 ]
 
-export const TreeComponent = () => {
-  
+const Node = ({ source:{icon, items, isAdmin }}) => {
+  const [hideComponent, setHideComponent] = useState(false)
   const style = styles()
-
   return (
     <div className={style.wrapper}>
-      <div className={style.dropdown}>
-        <FontAwesomeIcon icon={faCaretDown} size="1x" />
-      </div>
-      <div>
-        <div className={style.treeWrapper}>
-          <FontAwesomeIcon icon={faUser} size="1x" />
-          Manager          
+        <div className={style.dropdown}>
+          <FontAwesomeIcon 
+            onClick={() => setHideComponent(!hideComponent)} 
+            icon={ hideComponent ? faCaretUp : faCaretDown} 
+            size="1x" />
         </div>
+        <div>
+          <div className={style.treeWrapper}>
+            <FontAwesomeIcon icon={icon} size="2x" />
+            Manager          
+          </div>
+        </div>
+        <div className={style.options}>
+          <FontAwesomeIcon icon={faRemove} size="1x" />
+          <FontAwesomeIcon icon={faEdit} size="1x" />
+          {isAdmin ? <FontAwesomeIcon icon={faPlus} size="1x" /> : null}
+        </div>
+
+        {items.map( item => {
+          return (
+            <Node source={item}/>
+          )
+        })}
+
       </div>
-      <div className={style.options}>
-        {/* <Tooltip content="This is some tooltip content" > */}
-          <FontAwesomeIcon icon={faInfo} size="1x" />
-        {/* </Tooltip> */}
-        <FontAwesomeIcon icon={faPlus} size="1x" />
-      </div>
+  )
+}
+
+export const TreeComponent = () => {
+
+  const style = styles()
+  return (
+    <div className={style.wrapper}>
+      <Node source={Data}/>
     </div>
   )
 }
---
+++ src/mocks/data.tsx
@@ -0,0 +1,18 @@
+import {
+  faUser,
+  faCode,
+  faGear,
+  faCaretDown,
+  faInfo,
+  faPlus,
+  faRemove,
+  faCaretUp,
+  faEdit
+} from '@fortawesome/free-solid-svg-icons'
+
+export const Data = {
+    type: 'Manager',
+    isAdmin:true,
+    items: [],
+    icon: faUser
+}
